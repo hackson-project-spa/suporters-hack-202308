ac21f2072b6480d8f7e25cb69e1b3ff836b367d2
OtakeShunsuke <s.ootake.f3@icloud.com>
2023-07-30 22:49:09 +0900

    add: 新機能用ファイルのの追加

---
+++ app/react-app/src/FileRegister.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import { Box } from "@chakra-ui/react";
+
+class FileRegister extends React.Component {
+  render() {
+    return (
+      <Box>
+        <Header />
+        {/* <></> */}
+      </Box>
+    );
+  }
+}
+
+export default FileRegister;
---
+++ app/react-app/src/FileTable.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import { Box } from "@chakra-ui/react";
+
+class FileTable extends React.Component {
+  render() {
+    return (
+      <Box>
+        <Header />
+        {/* <></> */}
+      </Box>
+    );
+  }
+}
+
+export default FileTable;
