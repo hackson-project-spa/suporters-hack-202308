fd1d2637d02cfc21c700b07db68ccbabae9898a6
OtakeShunsuke <s.ootake.f3@icloud.com>
2023-07-31 10:31:07 +0900

    feat: タグ名称編集画面ポップアップの追加

--- app/react-app/src/TabMain.js
+++ app/react-app/src/TabMain.js
@@ -1,5 +1,23 @@
 import React from "react";
-import { Box, Button, FormLabel, HStack, Input, Stack, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormLabel,
+  HStack,
+  Input,
+  Stack,
+  Text,
+  VStack,
+  FormControl,
+  ButtonGroup,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import FocusLock from "react-focus-lock";
 
 class TabMain extends React.Component {
   render() {
@@ -71,6 +89,63 @@ class TabMain extends React.Component {
       },
     ];
 
+    // 1. Create a text input component
+    const TextInput = React.forwardRef((props, ref) => {
+      return (
+        <FormControl>
+          <FormLabel htmlFor={props.id}>{props.label}</FormLabel>
+          <Input ref={ref} id={props.id} {...props} />
+        </FormControl>
+      );
+    });
+
+    // 2. Create the form
+    const Form = ({ firstFieldRef, onCancel, tagId, tagName }) => {
+      return (
+        <Stack spacing={4}>
+          <TextInput label="タグ名称" id={tagId} ref={firstFieldRef} defaultValue={tagName} />
+          <ButtonGroup>
+            <Button variant="outline" onClick={onCancel}>
+              キャンセル
+            </Button>
+            <Button colorScheme="teal">更新</Button>
+          </ButtonGroup>
+        </Stack>
+      );
+    };
+
+    const PopoverForm = ({ tagId, tagName }) => {
+      const { onOpen, onClose, isOpen } = useDisclosure();
+      const firstFieldRef = React.useRef(null);
+
+      return (
+        <>
+          {/* <Box display="inline-block" mr={3}>
+            {tagName}
+          </Box> */}
+          <Popover
+            isOpen={isOpen}
+            initialFocusRef={firstFieldRef}
+            onOpen={onOpen}
+            onClose={onClose}
+            placement="right"
+            closeOnBlur={false}
+          >
+            <PopoverTrigger>
+              <Button size="sm">編集</Button>
+            </PopoverTrigger>
+            <PopoverContent p={5}>
+              <FocusLock returnFocus persistentFocus={false}>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <Form firstFieldRef={firstFieldRef} onCancel={onClose} tagId={tagId} tagName={tagName} />
+              </FocusLock>
+            </PopoverContent>
+          </Popover>
+        </>
+      );
+    };
+
     return (
       <VStack>
         <Box w="50vw" border="1px solid #00ffff" p={4} mt={4}>
@@ -92,7 +167,7 @@ class TabMain extends React.Component {
                   <Text w="38vw" fontSize="sm">
                     {post.title}
                   </Text>
-                  <Button size="sm">編集</Button>
+                  <PopoverForm tagId={post.id} tagName={post.title} />
                   <Button size="sm">削除</Button>
                 </HStack>
               </Stack>
