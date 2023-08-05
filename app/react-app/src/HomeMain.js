9762d9d797fd98011f75c3c5adba622252fddce1
kotatakehara <kotatakehara@gmail.com>
2023-08-05 21:21:27 +0900

    feet:ホームの各ボタンをカード化

--- app/react-app/src/HomeMain.js
+++ app/react-app/src/HomeMain.js
@@ -1,58 +1,72 @@
 import React from "react";
-import { Box, Flex, Center, Text, HStack, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Center,
+  Text,
+  HStack,
+  SimpleGrid,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import { Link as LinkRouter } from "react-router-dom";
 
-class HomeMain extends React.Component {
-  render() {
-    return (
-      <Box>
-        <Box p={20} bg={"##ff0000"}>
-          <Center>
-            <Text fontSize="xl">タイトルロゴ</Text>
-          </Center>
-        </Box>
-        <Box p={20}>
-          <Center>
-            <Text fontSize="xl">概要</Text>
-          </Center>
-        </Box>
-        <Box p={20}>
-          <Center>
-            <Flex>
-              <HStack spacing={"50px"}>
-                <LinkRouter to="/tag">
-                  <Box bg={"teal"} p={10} rounded="base">
-                    <Text fontSize="xl" color={"white"}>
-                      タグ登録
-                    </Text>
-                  </Box>
-                </LinkRouter>
-                <Link href="https://github.com/hackson-project-spa/proctice-hack-2/blob/devel/app/react-app/src/App.js">
-                  <Box bg={"teal"} p={10} rounded="base">
-                    <Text fontSize="xl" color={"white"}>
-                      ボタン
-                    </Text>
-                  </Box>
-                </Link>
-
-                <Box
-                  as="a"
-                  bg={"teal"}
-                  p={10}
-                  rounded="base"
-                  href="https://github.com/hackson-project-spa/proctice-hack-2/blob/devel/app/react-app/src/App.js"
-                >
-                  <Text fontSize="xl" color={"white"}>
-                    ボタン
-                  </Text>
-                </Box>
-              </HStack>
-            </Flex>
-          </Center>
-        </Box>
+function HomeMain() {
+  const buttonName = [
+    {
+      name: "ファイル検索",
+      path: "/index",
+      color: "",
+    },
+    {
+      name: "ファイル登録",
+      path: "/register",
+    },
+    {
+      name: "タグ登録",
+      path: "/tag",
+    },
+  ];
+  return (
+    <Box>
+      <Box p={20} bg={"##ff0000"}>
+        <Center>
+          <Text fontSize="xl">タイトルロゴ</Text>
+        </Center>
       </Box>
-    );
-  }
+      <Box p={20}>
+        <Center>
+          <Text fontSize="xl">概要</Text>
+        </Center>
+      </Box>
+      <Box p={20}>
+        <Center>
+          <SimpleGrid spacing={4} templeateColumns="repeat(auto-fill, minmax(200px, 1fr))">
+            <HStack spacing={"50px"}>
+              {buttonName.map((button) => (
+                <Card>
+                  <CardHeader>
+                    <Heading size="md"> {button.name}</Heading>
+                  </CardHeader>
+                  <CardBody>
+                    <Text>View a summary of all your customers over the last month.</Text>
+                  </CardBody>
+                  <CardFooter>
+                    <LinkRouter to={button.path}>
+                      <Button>{button.name}</Button>
+                    </LinkRouter>
+                  </CardFooter>
+                </Card>
+              ))}
+            </HStack>
+          </SimpleGrid>
+        </Center>
+      </Box>
+    </Box>
+  );
 }
 
 export default HomeMain;
